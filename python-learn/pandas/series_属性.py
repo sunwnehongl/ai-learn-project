a1e94cb52574885eef90941cef27f67028cadd82
sunwenhong <sunwenhongl@163.com>
2026-01-06 23:30:16 +0800

    numpy数学函数学习

--- python-learn/pandas/series_属性.py
+++ python-learn/pandas/series_属性.py
@@ -13,3 +13,11 @@ print("ndim:",s.ndim)
 print("size:",s.size)
 # dtype属性
 print("dtype:",s.dtype)
+# loc 显示索引,标签索引或者切片 iloc隐式索引，按位置索引或者切片
+print(s.iloc[0:2])
+# at 按标签访问单个原始，iat按位置访问当个元素
+print("第2个元素： ",s.at[1])
+
+# 数据访问
+print(s[2])
+print(s[s<4])
